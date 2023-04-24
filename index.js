70298a231ce66a8d297b5726d5295963ad9ae8ff
Naomi Peña <naosan25@protonmail.com>
2023-04-24 11:58:03 -0600

    feat: mdLinks

--- index.js
+++ index.js
@@ -1,75 +1,69 @@
-const { 
+const {
   existsRoute,
-  getAbsolutePath, 
-  fileExt, 
-  readFiles, 
-  extractLinks, 
+  getAbsolutePath,
+  fileExt,
+  readFiles,
+  extractLinks,
   urlToObjects,
   validUrl,
 } = require('./api.js');
 
-
-// Definimos la función mdLinks que recibe 2 parámetros y devuelve una nueva promesa (asíncrona)
 const mdLinks = (route, options) => {
-  // nota las opciones son objetos
-  // La nueva promesa toma un callback con sus parámetros-funciones resolve y reject (relacionados a then y catch)
   return new Promise(async (resolve, reject) => {
-    // nota: Promise es un constructor que recibe una función callback que tiene resolve y reject como parámetros
-    // A) Identificar si la ruta existe
     if (!existsRoute(route)) {
       // B) Si no existe se rechaza la promesa (reject) 
       reject('The route does not exist');
       // reject se puede llamar con un argumento para rechazar
       return; // patron de early return que asegura que no continue ejecutando la función
-    } 
+    }
     // Evaluar si la ruta es absoluta, en caso de que no convertirla
     const newRoute = getAbsolutePath(route);
     // Revisar si la ruta absoluta es un file o directorio
     // En caso de que sea un directorio: filtra archivos md y resuelve un Array de objetos(links)
     let mdFiles = [];
-   
     if (fileExt(newRoute)) {
       mdFiles.push(newRoute);
     }
-    // Aquí añadimos el manejo de directorios (pendiente)
-   
     try {
       const fileContents = await Promise.all(
         mdFiles.map((file) => readFiles(file))
-      );
-    
-    
-    const allLinks = fileContents.flatMap((fileContent)) => 
-      extractLinks(fileContent)
-    );
+      )
 
-    const linkObjects = allLinks.map((link, index) => {
-      return{
-        href:
-        text:
-        file:
-      };
-    });
+      const allLinks = fileContents.flatMap((fileContent) => {
+        extractLinks(fileContent)
+      });
 
-    if (options && options.validate) {
-      const validatedLinks = await validUrl(linkObjects);
-      resolve(validatedLinks);
-    } else {
-      resolve(linkObjects);
+      const linkObjects = allLinks.map((link, index) => {
+        return {
+          href: link.slice(link.indexOf("](h") + 2, -1),
+          text: link.slice(1, link.indexOf("]")),
+          file: mdFiles[index],
+        };
+      });
+
+      if (options && options.validate) {
+        const validatedLinks = await validUrl(linkObjects);
+        resolve(validatedLinks);
+      } else {
+        resolve(linkObjects);
+      }
     }
-  } catch (error) {
-    reject("Error procesando archivos md / Error processing Markdown files");
-  }
-});
-};
+    catch (error) {
+      reject("Error procesando archivos md / Error processing Markdown files");
+    }
+
+  })
+}
 
 //console.log(existsRoute);
 
-mdLinks('C:\\Users\\Naomi\\DEV003-md-links\\README.md').then( () => {
-
-}).catch( (error) => {
-  console.log(error);
+mdLinks('C:\\Users\\Naomi\\DEV003-md-links\\README.md', { validate: true })
+.then((result) => {
+  console.log(result);
 })
+.catch((error) => {
+  console.log(error);
+});
 
 // resolve se va a invocar al final
 // nota una vez terminada la función mdLinks, se va a ejecutar en CLI (donde se imprime con console.log)
@@ -79,5 +73,6 @@ module.exports = () => {
   // ...
   mdLinks
 };
+
 
 
