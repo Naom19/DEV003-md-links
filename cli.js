bc00f58809d78aa0eb58c064fe665fffaca924ee
Naomi Peña <naosan25@protonmail.com>
2023-04-25 13:30:27 -0600

    feat: fn para total, unique y broken links

--- cli.js
+++ cli.js
@@ -1,8 +1,36 @@
 const { mdLinks } = require('./index');
-mdLinks('./notvalidroute/').then(() => {
+// fn para obtener el valor del path
+
+/* mdLinks('./notvalidroute/').then(() => {
 
 })
 .cach((error) => {
     console.log(error);
 });
-console.log()
+console.log() */
+
+const path = process.argv[2]
+const validate = process.argv.includes('--validate') || process.argv.includes('--v');
+const stats = process.argv.includes('--stats') || process.argv.includes('--s');
+// fn para obtener el valor de options
+const options = { validate, stats };
+
+//fn para total links
+const totalLinks = (link) => {
+    const total = link.length;
+    return total;
+};
+
+//fn para uniqueLinks
+const uniqueLinks = (link) => {
+    const href = link.map(links => links.href);
+    return uniqueHrefs.size;
+};
+
+//fn para links rotos
+const brokenLinks = (link) => {
+    const failMessage = link.filter(links => links.ok ==='fail');
+    const uniqueFails = new Set(failMessage);
+    return uniqueFails.size;
+};
+
